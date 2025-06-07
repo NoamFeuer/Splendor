2bf954f4b0a738862657e7fc0891b08d14acac9e
Noam <pleasant.fir3@gmail.com>
2025-06-07 20:22:21 +0000

    finish Makefile and add raylib test

--- Makefile
+++ Makefile
@@ -1,12 +1,26 @@
 NAME = splendor
 CXX = c++
-FLAGS = -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+CXXFLAGS = -std=c++17 -Wall -Wextra -O2
+LDFLAGS = -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 
 SRCS = main.cpp
-
 OBJ_DIR = obj
 OBJS = $(addprefix $(OBJ_DIR)/, $(SRCS:.cpp=.o))
 
 all: $(NAME)
 
 $(NAME): $(OBJS)
+	$(CXX) $(OBJS) -o $(NAME) $(LDFLAGS)
+
+# Compile each .cpp to .o in obj/
+$(OBJ_DIR)/%.o: %.cpp | $(OBJ_DIR)
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+# Create obj directory if it doesn't exist
+$(OBJ_DIR):
+	mkdir -p $(OBJ_DIR)
+
+clean:
+	rm -rf $(OBJ_DIR) $(NAME)
+
+.PHONY: all clean
--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
-#include <iostream>
+#include "raylib.h"
 
-int main()
-{
-    std::cout << "Hello cats!" << std::endl;
+int main() {
+    // Initialize the window
+    InitWindow(800, 600, "raylib C++ Example");
 
+    // Set the target FPS
+    SetTargetFPS(60);
 
+    // Main game loop
+    while (!WindowShouldClose()) {
+        // Start drawing
+        BeginDrawing();
+
+        // Clear the background
+        ClearBackground(RAYWHITE);
+
+        // Draw some text
+        DrawText("Hello, raylib!", 350, 280, 20, DARKGRAY);
+
+        // Finish drawing
+        EndDrawing();
+    }
+
+    // Close the window
+    CloseWindow();
 
     return 0;
 }
